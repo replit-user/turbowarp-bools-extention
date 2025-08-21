4e13d3f1360083019276448055e5ed0a0c36a1df
replit-user <umyashinderu@gmail.com>
2025-08-21 09:50:43 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,115 +1,120 @@
 class BoolExtension {
-  constructor() {
-    this.variables = {};
-  }
+    constructor(runtime) {
+        this.runtime = runtime; // store runtime for variable access
+    }
 
-  getInfo() {
-    return {
-      id: 'boolext',
-      name: 'Bools',
-      color1: '#2e577eff', // lighter blue
-      color2: '#0d67c2ff', // slightly darker border
-      color3: '#033f77ff', // highlight
-      blocks: [
-        {
-          opcode: 'trueBlock',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: 'true',
-        },
-        {
-          opcode: 'falseBlock',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: 'false',
-        },
-        {
-          opcode: 'notBlock',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: 'not [VALUE]',
-          arguments: {
-            VALUE: {
-              type: Scratch.ArgumentType.BOOLEAN,
-              defaultValue: true
-            }
-          }
-        },
-        '---',
-        {
-          opcode: 'setBool',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'set bool [NAME] to [VALUE]',
-          arguments: {
-            NAME: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'flag'
-            },
-            VALUE: {
-              type: Scratch.ArgumentType.BOOLEAN,
-              defaultValue: true
-            }
-          }
-        },
-        {
-          opcode: 'getBool',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: 'bool [NAME]',
-          arguments: {
-            NAME: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'flag'
-            }
-          }
-        },
-        {
-            opcode:"ToggleBoolVar",
-            blockType:Scratch.BlockType.COMMAND,
-            text: 'toggle [BOOL_VAR]',
-            arguments:{
-                "BOOL_VAR":{
-                    type:Scratch.ArgumentType.STRING,
-                    defaultValue:'flag'
+    getInfo() {
+        return {
+            id: 'boolext',
+            name: 'Bools',
+            color1: '#2e577eff', // lighter blue
+            color2: '#0d67c2ff', // slightly darker border
+            color3: '#033f77ff', // highlight
+            blocks: [
+                {
+                    opcode: 'trueBlock',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: 'true',
+                },
+                {
+                    opcode: 'falseBlock',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: 'false',
+                },
+                {
+                    opcode: 'notBlock',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: 'not [VALUE]',
+                    arguments: {
+                        VALUE: {
+                            type: Scratch.ArgumentType.BOOLEAN,
+                            defaultValue: true
+                        }
+                    }
+                },
+                '---',
+                {
+                    opcode: 'setBool',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'set bool [NAME] to [VALUE]',
+                    arguments: {
+                        NAME: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'flag'
+                        },
+                        VALUE: {
+                            type: Scratch.ArgumentType.BOOLEAN,
+                            defaultValue: true
+                        }
+                    }
+                },
+                {
+                    opcode: 'getBool',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: 'bool [NAME]',
+                    arguments: {
+                        NAME: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'flag'
+                        }
+                    }
+                },
+                {
+                    opcode: "ToggleBoolVar",
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'toggle [BOOL_VAR]',
+                    arguments: {
+                        "BOOL_VAR": {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'flag'
+                        }
+                    }
                 }
-            }
-        },
-        {
-    opcode: 'listBools',
-    blockType: Scratch.BlockType.REPORTER,
-    text: 'all bools'
-}
+            ]
+        };
+    }
 
-      ]
-    };
-  }
+    // helper: get or create a Scratch variable by name
+    getProjectVariable(name) {
+        const target = this.runtime.getTargetForStage();
+        let variable = target.lookupVariableByNameAndType(name, '');
+        if (!variable) {
+            variable = target.createVariable(name, '', false); // false = not cloud
+        }
+        return variable;
+    }
 
-  trueBlock() {
-    return true;
-  }
+    // boolean literal blocks
+    trueBlock() {
+        return true;
+    }
 
-  falseBlock() {
-    return false;
-  }
+    falseBlock() {
+        return false;
+    }
 
-  notBlock(args) {
-    return !args.VALUE;
-  }
+    // logical NOT
+    notBlock(args) {
+        return !args.VALUE;
+    }
 
-  setBool(args) {
-    this.variables[args.NAME] = args.VALUE;
-  }
+    // set a Scratch project variable to a boolean value
+    setBool(args) {
+        const variable = this.getProjectVariable(args.NAME);
+        variable.value = Boolean(args.VALUE);
+    }
 
-  getBool(args) {
-    return Boolean(this.variables[args.NAME]);
-  }
-  ToggleBoolVar(args) {
-  // Default to false if variable doesn't exist
-  const current = Boolean(this.variables[args.BOOL_VAR]);
-  this.variables[args.BOOL_VAR] = !current;
-}
-  listBools() {
-    return Object.keys(this.variables).join(', ');
-}
+    // read a Scratch project variable as boolean
+    getBool(args) {
+        const variable = this.getProjectVariable(args.NAME);
+        return Boolean(variable.value);
+    }
 
-
+    // toggle a Scratch project variable
+    ToggleBoolVar(args) {
+        const variable = this.getProjectVariable(args.BOOL_VAR);
+        variable.value = !Boolean(variable.value);
+    }
 }
 
 Scratch.extensions.register(new BoolExtension());
-
